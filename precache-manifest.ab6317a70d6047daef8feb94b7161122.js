ee202309fa9aa766a1359d3c8b505e2b07beae9d
Chafik <gchafik@gmail.com>
2019-05-16 20:50:51 -0700

    Updates

---
+++ precache-manifest.ab6317a70d6047daef8feb94b7161122.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e86e93f2351d9d016993be6a200c7a60",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "08a2ffab7724b22187cc",
+    "url": "/clicky-game/static/css/main.a0734e40.chunk.css"
+  },
+  {
+    "revision": "4dbc779420a3e2162ebe",
+    "url": "/clicky-game/static/js/2.9590dc6d.chunk.js"
+  },
+  {
+    "revision": "08a2ffab7724b22187cc",
+    "url": "/clicky-game/static/js/main.04dadf9b.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
